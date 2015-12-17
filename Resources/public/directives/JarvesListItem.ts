c55cc0c8af8cdb94bfb54ec8270bb5c18b616a7e
Marc J. Schmidt <marc@marcjschmidt.de>
2015-12-18 00:53:23 +0100

    Added JarvesList splitter

--- Resources/public/directives/JarvesListItem.ts
+++ Resources/public/directives/JarvesListItem.ts
@@ -9,7 +9,7 @@ export default class JarvesListItem {
 
     protected element;
 
-    public constructor(protected $compile){
+    public constructor(protected $compile) {
 
     }
 
@@ -17,50 +17,70 @@ export default class JarvesListItem {
         var template = jarvesListController.getItemTemplateElement();
         this.element = element;
         element.empty();
-        var previousController;
-
-        if (element.prev() && (previousController = element.prev().controller('jarvesListItem'))) {
-            if (previousController) {
-                var previousSplitHtml = previousController.getSplitHtml();
-                var currentSplitHtml = this.getSplitHtml();
-                if (false !== previousSplitHtml && false !== currentSplitHtml) {
-                    if (currentSplitHtml !== currentSplitHtml) {
-                        this.injectSplit(currentSplitHtml);
-                    }
-                }
-            }
-        } else {
-            var currentSplitHtml = this.getSplitHtml();
-            if (false !== currentSplitHtml) {
-                this.injectSplit(currentSplitHtml);
-            }
-        }
 
         if (template) {
-            element.append(template);
+            var container = angular.element('<a class="jarves-list-item"></a>');
+            container.append(template);
+            element.append(container);
             this.$compile(element.children())(scope);
         } else {
             throw 'No <jarves-list-template></jarves-list-template> specified.';
         }
+
+        scope.$watch('item', () => this.updateSplit(), true);
+
     }
 
+    updateSplit() {
+        var previousController;
+
+        if (this.element.prev() && (previousController = this.element.prev().controller('jarvesListItem'))) {
+            var previousSplitHtml = previousController.getSplitHtml();
+            var currentSplitHtml = this.getSplitHtml();
+            if (null !== previousSplitHtml && null !== currentSplitHtml) {
+                if (previousSplitHtml !== currentSplitHtml) {
+                    this.injectSplit(currentSplitHtml);
+                }
+            }
+        } else {
+            var currentSplitHtml = this.getSplitHtml();
+            if (null !== currentSplitHtml) {
+                this.injectSplit(currentSplitHtml);
+            }
+        }
+    }
+
+    protected lastSplit = null;
+    protected lastSplitHtml = null;
+
     protected injectSplit(html:string) {
+
+        if (this.lastSplitHtml === html) {
+            return;
+        }
+
+        if (this.lastSplit) {
+            this.lastSplit.remove();
+        }
+
         var splitElement = angular.element(html);
         if (splitElement) {
             var split = angular.element('<jarves-list-item-split></jarves-list-item-split>');
             split.append(splitElement);
 
-            this.element.before(split);
+            this.element.prepend(split);
+            this.lastSplit = split;
+            this.lastSplitHtml = html;
         }
     }
 
-    public getSplitHtml():string|boolean {
+    public getSplitHtml():string {
         var text = this.element.text();
 
         if (text) {
-            '<b>' + text.trim()[0] + '</b>';
+            return '<b>' + text.trim()[0] + '</b>';
         }
 
-        return false;
+        return null;
     }
 }
