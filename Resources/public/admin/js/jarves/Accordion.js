75f8192cf75dbfe8b27af697f72ca62cc34cb580
Marc J. Schmidt <marc@marcjschmidt.de>
2016-04-09 11:38:40 +0200

    Updated copyright header

--- Resources/public/admin/js/jarves/Accordion.js
+++ Resources/public/admin/js/jarves/Accordion.js
@@ -1,3 +1,16 @@
+/*
+ * This file is part of Jarves.
+ *
+ * (c) Marc J. Schmidt <marc@marcjschmidt.de>
+ *
+ *     J.A.R.V.E.S - Just A Rather Very Easy [content management] System.
+ *
+ *     http://jarves.io
+ *
+ * To get the full copyright and license information, please view the
+ * LICENSE file, that was distributed with this source code.
+ */
+
 jarves.Accordion = new Class({
     Implements: Events,
 
--- Resources/public/admin/js/lib/DOMEvent.js
+++ Resources/public/admin/js/lib/DOMEvent.js
@@ -1,3 +1,16 @@
+/*
+ * This file is part of Jarves.
+ *
+ * (c) Marc J. Schmidt <marc@marcjschmidt.de>
+ *
+ *     J.A.R.V.E.S - Just A Rather Very Easy [content management] System.
+ *
+ *     http://jarves.io
+ *
+ * To get the full copyright and license information, please view the
+ * LICENSE file, that was distributed with this source code.
+ */
+
 DOMEvent.implement({
     isMiddleClick: function(){
         // Add which for click: 1 === left; 2 === middle; 3 === right
