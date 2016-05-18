a6d8536c190b12e300b65b438a31d487e98d4fc4
Marc J. Schmidt <marc@marcjschmidt.de>
2016-05-18 13:37:52 +0200

    remove web/cache of test suite

--- Tests/Integration/skeletion/web/cache/compressed-1667807f085f2ae257f008a991fd54e7.js
+++
@@ -1,8 +0,0 @@
-/* 1667807f085f2ae257f008a991fd54e7 */
-
-/* @TestBundle/page-response-test/javascript1.js */
-
-var a = 'abc';
-/* @TestBundle/page-response-test/javascript2.js */
-
-var b = 'cbd';
