c4e4c956b97da72dc20eda3a31a738492f13ad80
Philipp Bräutigam <philipp.braeutigam@googlemail.com>
2016-10-27 11:21:24 +0200

    Removed obsolete TinyMCE plugin

--- Resources/public/admin/js/jarves/FieldTypes/Wysiwyg.js
+++ Resources/public/admin/js/jarves/FieldTypes/Wysiwyg.js
@@ -37,7 +37,7 @@ jarves.FieldTypes.Wysiwyg = new Class({
                 plugins: [
                     "advlist autolink lists link image charmap print preview anchor",
                     "searchreplace visualblocks code fullscreen",
-                    "insertdatetime media table contextmenu paste moxiemanager"
+                    "insertdatetime media table contextmenu paste"
                 ],
                 toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
             },
